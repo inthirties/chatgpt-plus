0e921e09f94151e2faa142f3e1d51f501a8b0289
Liuyong <793875613@qq.com>
2023-07-23 12:50:42 +0800

    add

--- web/vue.config.js
+++ web/vue.config.js
@@ -18,7 +18,19 @@ module.exports = defineConfig({
     outputDir: 'dist',
     crossorigin: "anonymous",
     devServer: {
-        allowedHosts: ['192.168.52.102:5678'],
+        allowedHosts: ['localhost:5678'],
         port: 8888,
+		proxy: {
+      '/api': {
+        target: 'http://localhost:5678',//代理地址，这里设置的地址会代替axios中设置的baseURL
+        secure: false, //如果是https接口需要进行此配置 
+        changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
+        //pathRewrite方法重写url
+        pathRewrite: {
+          // '^/api': '/aaa' //如果没有pathRewrite属性 调用的接口就是http://cloud/api/xxx/xxxx 如果有 pathRewrite属性 调用的接口就是 http://cloud/aaa/xxx/xxxx
+		  '^/api': '/api' //如果没有pathRewrite属性 调用的接口就是http://cloud/api/xxx/xxxx 如果有 pathRewrite属性 调用的接口就是 http://cloud/aaa/xxx/xxxx
+         }
+        }
+      }    
     }
 })
